7fd631de2962ce579d40ba919615f0667b2f6b4a
hongze.chi <hongze.chi@shicaigj.com>
2016-10-14 12:45:16 +0800

    test disruptor

---
+++ src/main/java/chihz/bot/elder/DisruptorTest.java
@@ -0,0 +1,25 @@
+package chihz.bot.elder;
+
+/**
+ * Created by chihongze on 16/10/11.
+ */
+public class DisruptorTest {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class LongEvent {
+
+    private final long value;
+
+    public LongEvent(long value) {
+        this.value = value;
+    }
+
+    public long getValue() {
+        return this.value;
+    }
+}
